7aa3b41a76682f8b06b99bc7419b296b7285c6b1
Zeyuan <yinzeyuan1037@gmail.com>
2026-04-04 20:17:43 -0400

    get test results

--- test.sh
+++ test.sh
@@ -4,3 +4,5 @@ export PYTHONPATH="${ROOT}${PYTHONPATH:+:${PYTHONPATH}}"
 cd "$ROOT" || exit 1
 
 torchrun --nproc_per_node=4 ./test/test_hybrid_attn.py --sp_ulysses_degree 4 --ring_impl_type "basic" --attn_impl "paro"
+
+# torchrun --nproc_per_node=4 selector.py
